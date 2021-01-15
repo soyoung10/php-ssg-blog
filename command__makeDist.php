672abcdeb24219c9c1eb2b81caaa645265e03bfc
soyoung10 <soyoung03152@gmail.com>
2021-01-16 02:59:08 +0900

    html extraction

--- command__makeDist.php
+++ command__makeDist.php
@@ -1,3 +1,17 @@
 <?php
-echo 'hi';
- ?>
+$originFiles = [
+    "index.ssghtml.php",
+    "about.ssghtml.php",
+];
+
+foreach ( $originFiles as $originFile ) {
+    $distFileName = str_replace(".ssghtml.php", ".html", $originFile);
+    $command = "c:\\xampp\\php\\php.exe {$originFile} > {$distFileName}";
+    shell_exec($command);
+
+    $newSource = file_get_content($distFileName);
+    echo $newSource;
+    exit;
+    exit;
+}
+
