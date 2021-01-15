5e40ba1992fd7caac37fc096a040ec091d1c3e63
soyoung10 <soyoung03152@gmail.com>
2021-01-15 17:41:19 +0900

    modified

--- php/about.ssghtml.php
+++ php/about.ssghtml.php
@@ -3,15 +3,15 @@ require_once "head.php";
 ?>
 
 <section class="section-title con-min-width">
-      <h1 class="con">
-        <span>
-          <i class="fas fa-clock"></i>
-        </span>
-        <span>
-          LATEST ARTICLES
-        </span>
-      </h1>
-    </section>
+  <h1 class="con">
+    <span>
+      <i class="fas fa-clock"></i>
+    </span>
+    <span>
+      LATEST ARTICLES
+    </span>
+  </h1>
+</section>
 
 <?php
 require_once "foot.php";
