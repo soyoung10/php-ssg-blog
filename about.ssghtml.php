7bb5e8d360d59a9448058bd0dcbb0bc143d8270d
soyoung10 <soyoung03152@gmail.com>
2021-01-22 05:12:59 +0900

    중복된 하단바 삭제

--- about.ssghtml.php
+++ about.ssghtml.php
@@ -17,15 +17,6 @@ require_once "head.php";
 
   </main>
 
-  <footer class="bottom-bar con-min-width">
-    <div class="con flex jc-c">
-      <a href="#" class="logo">
-        <span><i class="fas fa-palette"></i><i class="fas fa-paint-brush"></i></span>
-        <span>DESIGN SheRRy</span>
-      </a>
-    </div>
-  </footer>
-
 </div>
 </body>
 </html>
