94ab384ac7c24326c02313b5f9b9817c79b2065b
soyoung10 <soyoung03152@gmail.com>
2021-01-26 17:40:00 +0900

    a link 수정

--- article_detail.ssghtml.php
+++ article_detail.ssghtml.php
@@ -26,7 +26,7 @@ $selectedArticle = &getArticleById($articleId);
 
 <section class="section-article-detail con-min-width padding-0-10">
   <div class="con">
-    <h1 class="article-list-box__title"><a href="article_detail_2.ssghtml.php"><?=$selectedArticle["title"]?></a></h1>
+    <h1 class="article-list-box__title"><?=$selectedArticle["title"]?></h1>
 
     <div class="article-list-box__reg-date"><?=$selectedArticle["regDate"]?></div>
 
@@ -39,29 +39,34 @@ $selectedArticle = &getArticleById($articleId);
       <script type="text/x-template"><?=$selectedArticle['body']?></script>
       <div class="toast-ui-viewer"></div>
     </div>
+
+    <div class="article-list-box__tags">
+      <?=getArticleTagsHtml($article["id"])?>
+    </div>
   </div>
 </section>
 
 
 
 <section class="section-article-reply-box padding-0-10 con-min-width">
-    <div class="con">
+  <div class="con">
     <div id="disqus_thread"></div>
 
     <script>
-        var disqus_config = function () {
-            this.page.url = 'https://b.sherryisprecious.com/article_detail_<?=$articleId?>.html';
-            this.page.identifier = 'article_detail_<?=$articleId?>.html';
-        };
-        (function() { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
+      var disqus_config = function () {
+        this.page.url = 'https://b.sherryisprecious.com/article_detail_<?=$articleId?>.html';
+        this.page.identifier = 'article_detail_<?=$articleId?>.html';
+      };
+      (function () { // DON'T EDIT BELOW THIS LINE
+        var d = document,
+          s = d.createElement('script');
         s.src = 'https://blogsherry-2.disqus.com/embed.js';
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
-        })();
+      })();
     </script>
-    </div>
+  </div>
 
-<?php
+  <?php
 require_once "foot.php";
 ?>
