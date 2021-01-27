ece3f30a1adba96764e467937377eeffd31f1017
soyoung10 <soyoung03152@gmail.com>
2021-01-28 01:37:21 +0900

    , add

--- data.php
+++ data.php
@@ -12,7 +12,7 @@ $siteThumbUrl = "https://soyoung10.github.io/php-ssg-blog/img/logo/design_sherry
 // 태그정보
 $tagInfos = [
     "js" => [
-        "pageThumbUrl" => "https://soyoung10.github.io/php-ssg-blog/img/logo/design_sherry-logo-2.png"
+        "pageThumbUrl" => "https://soyoung10.github.io/php-ssg-blog/img/logo/design_sherry-logo-2.png",
         //"pageDescription" => "2021년 특별기획, 자바스크립트 관련 강좌 입니다."
     ],
     "jquery" => [
