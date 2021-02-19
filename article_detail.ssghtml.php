475ff12892dd99aaa9d239c3ca621ae8b743be99
soyoung10 <soyoung03152@gmail.com>
2021-02-19 18:17:09 +0900

    beauty file

--- article_detail.ssghtml.php
+++ article_detail.ssghtml.php
@@ -27,7 +27,7 @@ require_once "head.php";
     <div class="con">
         <h1 class="article-list-box__title"><?=$selectedArticle["title"]?></h1>
         <div class="article-list-box__reg-date"><?=$selectedArticle["regDate"]?></div>
-        
+
         <div class="article-list-box__writer">
             <span><?=$selectedArticle["writerName"]?></span>
             <span><?=$selectedArticle["writerAvatar"]?></span>
@@ -35,7 +35,7 @@ require_once "head.php";
         <div class="article-list-box__tags">
             <?=getArticleTagsHtml($selectedArticle["id"])?>
         </div>
-        
+
         <div class="article-list-box__body">
             <script type="text/x-template"><?=$selectedArticle['body']?></script>
             <div class="toast-ui-viewer"></div>
@@ -44,20 +44,21 @@ require_once "head.php";
 </section>
 <section class="section-article-reply-box padding-0-10 con-min-width">
     <div class="con">
-    <div id="disqus_thread"></div>
-    
-    <script>
-        var disqus_config = function () {
-            this.page.url = 'https://b.oa.gg/article_detail_<?=$articleId?>.html';
-            this.page.identifier = 'article_detail_<?=$articleId?>.html';
-        };
-        (function() { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        s.src = 'https://phpblog-2.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-        })();
-    </script>
+        <div id="disqus_thread"></div>
+
+        <script>
+            var disqus_config = function () {
+                this.page.url = 'https://b.oa.gg/article_detail_<?=$articleId?>.html';
+                this.page.identifier = 'article_detail_<?=$articleId?>.html';
+            };
+            (function () { // DON'T EDIT BELOW THIS LINE
+                var d = document,
+                    s = d.createElement('script');
+                s.src = 'https://phpblog-2.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
     </div>
 </section>
 <?php
--- index.ssghtml.php
+++ index.ssghtml.php
@@ -4,47 +4,48 @@ require_once "head.php";
 $articles = &getArticles();
 ?>
 
-<link rel="stylesheet" href="css/index.css">
-<script src="js/index.js" defer></script>
+ <link rel="stylesheet" href="css/index.css">
+ <script src="js/index.js" defer></script>
 
-<section class="section-title con-min-width">
-  <h1 class="con">
-    <span>
-      <i class="fas fa-clock"></i>
-    </span>
-    <span>
-      LATEST ARTICLES
-    </span>
-  </h1>
-</section>
+ <section class="section-title con-min-width">
+   <h1 class="con">
+     <span>
+       <i class="fas fa-clock"></i>
+     </span>
+     <span>
+       LATEST ARTICLES
+     </span>
+   </h1>
+ </section>
 
-<section class="section-latest-articles con-min-width">
-  <div class="con">
-    <div class="article-list-box">
-      <ul>
-        <?php foreach ( $articles as $article ) { ?>
-        <li>
-        <h1 class="article-list-box__title"><a href="<?=getArticleLink($article["id"])?>"><?=$article["title"]?></a></h1>
-          <div class="article-list-box__reg-date"><?=$article["regDate"]?></div>
-          <div class="article-list-box__writer">
-            <span><?=$article["writerName"]?></span>
-            <span><?=$article["writerAvatar"]?></span>
-          </div>
-          <div class="article-list-box__tags">
-            <?=getArticleTagsHtml($article["id"])?>
-          </div>
-          <div class="article-list-box__body">
-            <script type="text/x-template"><?=$article['body']?></script>
-            <div class="toast-ui-viewer"></div>
-          </div>
-        </li>
-        <?php } ?>
-      </ul>
-    </div>
-  </div>
-</section>
+ <section class="section-latest-articles con-min-width">
+   <div class="con">
+     <div class="article-list-box">
+       <ul>
+         <?php foreach ( $articles as $article ) { ?>
+         <li>
+           <h1 class="article-list-box__title"><a href="<?=getArticleLink($article["id"])?>"><?=$article["title"]?></a>
+           </h1>
+           <div class="article-list-box__reg-date"><?=$article["regDate"]?></div>
+           <div class="article-list-box__writer">
+             <span><?=$article["writerName"]?></span>
+             <span><?=$article["writerAvatar"]?></span>
+           </div>
+           <div class="article-list-box__tags">
+             <?=getArticleTagsHtml($article["id"])?>
+           </div>
+           <div class="article-list-box__body">
+             <script type="text/x-template"><?=$article['body']?></script>
+             <div class="toast-ui-viewer"></div>
+           </div>
+         </li>
+         <?php } ?>
+       </ul>
+     </div>
+   </div>
+ </section>
 
 
-<?php
+ <?php
 require_once "foot.php";
 ?>
