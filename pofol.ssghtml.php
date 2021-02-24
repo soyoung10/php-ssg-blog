fcab60c0a6591b5cc3f48e9ca210622b855850e3
soyoung10 <soyoung03152@gmail.com>
2021-02-25 04:28:28 +0900

    .

--- pofol.ssghtml.php
+++ pofol.ssghtml.php
@@ -6,20 +6,9 @@ require_once "head.php";
 <!DOCTYPE html>
 <html lang="ko">
 
-
 <head>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-680JQ7V954"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-680JQ7V954');
-</script>
-
-  <meta charset="UTF-8">
+<meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
 <link rel="stylesheet" href="css/pofol.css">
