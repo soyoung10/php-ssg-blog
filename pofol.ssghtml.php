38c7ea0b90f4d24c127658a23bf266bf76c9d6a6
soyoung10 <soyoung03152@gmail.com>
2021-02-25 04:02:44 +0900

    update pofol.html

--- pofol.ssghtml.php
+++ pofol.ssghtml.php
@@ -3,6 +3,8 @@ require_once "data.php";
 require_once "head.php";
 ?>
 
+<link rel="stylesheet" href="css/pofol.css">
+<script src="js/pofol.js"></script>
 
 <link rel="stylesheet" href="css/common.css">
 <script src="js/common.js"></script>
@@ -14,8 +16,37 @@ require_once "head.php";
   </h1>
 </section>
 
-<section class="section-main con-min-width">
-  <a href="https://b.sherryisprecious.com/pofol/process/interaction.html">PortFolio01</a>
+<section class="section-box">
+  <ul>
+    <li>
+      <span></span>
+      <div class="title">Mobile App UI/UX</div>
+      <div class="info">Å©¶ó¿ìµå ÆÝµù ÇÃ·§Æû 'MOEZLE'</div>
+      <div class="link"><a href="https://b.sherryisprecious.com/pofol/process/interaction.html">link >></a></div>
+      <div class="time">
+        <span>Fub,22<sup>th</sup></span>
+        <span></span>
+      </div>
+    </li>
+    <li>
+      <span></span>
+      <div class="title">TITLE</div>
+      <div class="info">INFOMATION</div>
+      <div class="link">LINK>></div>
+      <div class="time">
+        <span>MONTH,DAY<sup>th</sup></span>
+      </div>
+    </li>
+    <li>
+      <span></span>
+      <div class="title">TITLE</div>
+      <div class="info">INFOMATION</div>
+      <div class="link">LINK>></div>
+      <div class="time">
+        <span>MONTH,DAY<sup>th</sup></span>
+      </div>
+    </li>
+  </ul>
 </section>
 
 <?php
