aeff7e31f065dcdb7b8bebb31fd1360b13803282
soyoung10 <soyoung03152@gmail.com>
2021-03-12 22:17:02 +0900

    /script 추가_utf-8

--- pofol.ssghtml.php
+++ pofol.ssghtml.php
@@ -12,7 +12,7 @@ require_once "head.php";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="stylesheet" href="/css/pofol.css">
-  <script src="/js/pofol.js"></script>
+  <script src="/js/pofol.js" defer></script>
 
 </head>
 
