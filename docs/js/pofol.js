18e2fdd1e097090ac6d73b67acb6182d872d2ae0
soyoung10 <soyoung03152@gmail.com>
2021-03-12 15:06:58 +0900

    new window

---
+++ docs/js/pofol.js
@@ -0,0 +1,5 @@
+var newWindow = window.open("about:blank");
+
+newWindow.location.href = 'https://b.sherryisprecious.com/pofol/process/interaction.html';
+newWindow.location.href = 'https://b.sherryisprecious.com/pofol/process/process.html';
+newWindow.location.href = 'https://b.sherryisprecious.com/pofol/web/index.html';
