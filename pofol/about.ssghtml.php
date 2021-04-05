14e2246ab2e6c239aa3e10ec4e88047e858dd6d3
soyoung10 <soyoung03152@gmail.com>
2021-04-05 18:47:32 +0900

    포트폴리오

---
+++ pofol/about.ssghtml.php
@@ -0,0 +1,12 @@
+<?php
+$pageCode = 'about';
+$pageTitle = "ABOUT";
+require_once "head.php";
+?>
+<link rel="stylesheet" href="about.css">
+<script src="about.js" defer></script>
+<h1>ABOUT</h1>
+
+<?php
+require_once "foot.php";
+?>
---
+++ pofol/index.ssghtml.php
@@ -0,0 +1,12 @@
+<?php
+$pageCode = 'index';
+$pageTitle = "È¨";
+require_once "head.php";
+?>
+<link rel="stylesheet" href="index.css">
+<script src="index.js" defer></script>
+<h1>¸ÞÀÎ></h1>
+
+<?php
+require_once "foot.php";
+?>
